d4d24fc0c6b35f4b99b4b4ed7c3f9598e7a64d03
usyyy <usmaanalii@hotmail.co.uk>
2017-10-26 19:56:18 +0100

    Worked through Mike Bostocks update tutorial

---
+++ general-update-patterns/1/script.js
@@ -0,0 +1,38 @@
+const alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+
+const svg = d3.select('svg')
+const width = +svg.attr('width');
+const height = +svg.attr('height');
+const g = svg.append('g')
+  .attr('transform', `translate(32, ${height / 2})`);
+  
+const update = data => {
+  // Join new data with old elements (if any)
+  const text = g.selectAll('text')
+    .data(data);
+  
+  // Update old elements as needed
+  text.attr('class', 'update');
+  
+  // Create new elements as needed
+  // Merges new elements
+  // Applies operations to both new and updated
+  text.enter()
+    .append('text')
+    .attr('class', 'enter')
+    .attr('x', (d, i) => i * 32)
+    .attr('dy', '.35em')
+    .merge(text)
+    .text(d => d);
+  
+  // Remove old elements
+  text.exit().remove();
+};
+
+update(alphabet);
+
+d3.interval(() => {
+  update(d3.shuffle(alphabet)
+    .slice(0, Math.floor(Math.random() * 26))
+    .sort());
+}, 1500);
